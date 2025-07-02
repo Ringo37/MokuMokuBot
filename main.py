dc85e7048a74ae20a584da809716b6d727b89455
Ringo37 <yajima.haruhiko@gmail.com>
2025-07-03 00:21:37 +0900

    fix

--- main.py
+++ main.py
@@ -52,7 +52,10 @@ async def on_voice_state_update(member, before, after):
 
     # 入室処理
     if after.channel is not None:
-        if after.channel.id == VOICE_CHANNEL_ID and before.channel.id != VOICE_CHANNEL_ID:
+        if before.channel is not None:
+            if before.channel.id == VOICE_CHANNEL_ID:
+                return
+        if after.channel.id == VOICE_CHANNEL_ID:
             log = VoiceLog(user_id=user.id, join_time=datetime.now(JST))
             session.add(log)
             session.commit()
